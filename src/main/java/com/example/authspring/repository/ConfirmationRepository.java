24af21fd2b93441df604a573fa6807f5f537b997
sinemoraa <jonshilin.gg@gmail.com>
2025-07-22 16:47:41 +0300

    refactor: remove unused User import from ConfirmationRepository

--- src/main/java/com/example/authspring/repository/ConfirmationRepository.java
+++ src/main/java/com/example/authspring/repository/ConfirmationRepository.java
@@ -2,7 +2,6 @@ package com.example.authspring.repository;
 
 import com.example.authspring.enums.ConfirmationAction;
 import com.example.authspring.model.Confirmation;
-import com.example.authspring.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
