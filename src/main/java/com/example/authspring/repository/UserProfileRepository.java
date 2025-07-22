374adb16ca23dde6f64fadaae7f60d8d6d49c574
sinemoraa <jonshilin.gg@gmail.com>
2025-07-22 16:38:58 +0300

    feat: add UserProfileRepository for managing UserProfile entities

---
+++ src/main/java/com/example/authspring/repository/UserProfileRepository.java
@@ -0,0 +1,15 @@
+package com.example.authspring.repository;
+
+import com.example.authspring.model.User;
+import com.example.authspring.model.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+    Optional<UserProfile> findByUser(User user);
+
+    void deleteByUser(User user);
+}
