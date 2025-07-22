652b3ce475f8a7675321a5b1ef458e0690a7b6ab
sinemoraa <jonshilin.gg@gmail.com>
2025-07-22 19:36:58 +0300

    feat: add method to find all refresh tokens by user in RefreshTokenRepository

--- src/main/java/com/example/authspring/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/authspring/repository/RefreshTokenRepository.java
@@ -5,14 +5,11 @@ import com.example.authspring.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByToken(String token);
-
-    int deleteByUser(User user);
-
-    long deleteAllByExpiryDateBeforeOrRevokedTrue(Instant now);
+    List<RefreshToken> findAllByUser(User user);
 }
