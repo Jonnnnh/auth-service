38764d801eaf62b4afb53ac123eebb72af97d06a
sinemoraa <jonshilin.gg@gmail.com>
2025-07-22 17:43:45 +0300

    feat: change userId type to UUID in RegistrationResponse

--- src/main/java/com/example/authspring/dto/response/RegistrationResponse.java
+++ src/main/java/com/example/authspring/dto/response/RegistrationResponse.java
@@ -3,6 +3,8 @@ package com.example.authspring.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -12,7 +14,7 @@ import lombok.*;
 public class RegistrationResponse {
 
     @Schema(description = "Идентификатор пользователя", example = "1")
-    private Long userId;
+    private UUID userId;
 
     @Schema(description = "Сообщение", example = "Регистрация успешна. Проверьте свою почту для подтверждения")
     private String message;
