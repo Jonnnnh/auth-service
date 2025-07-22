c5ba8f1230a4f49641fa7116a74a8046d879f748
sinemoraa <jonshilin.gg@gmail.com>
2025-07-22 16:37:41 +0300

    feat: rename OpenApiConfig package to openapi for better organization

--- src/main/java/com/example/authspring/config/openapi/OpenApiConfig.java
+++ src/main/java/com/example/authspring/config/openapi/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package com.example.authspring.config;
+package com.example.authspring.config.openapi;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
