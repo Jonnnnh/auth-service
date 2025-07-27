60c3a9199c41f66f294f56661d1cc64466f434a4
sinemoraa <jonshilin.gg@gmail.com>
2025-07-27 17:42:11 +0300

    feat: update docker-compose.yml to remove mail configuration; enhance openApiConfig with security requirements for jwt

--- src/main/java/com/example/authspring/config/openapi/OpenApiConfig.java
+++ src/main/java/com/example/authspring/config/openapi/OpenApiConfig.java
@@ -1,9 +1,11 @@
 package com.example.authspring.config.openapi;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,13 +16,15 @@ import org.springframework.context.annotation.Configuration;
                 version = "1.0.0",
                 description = "Сервис авторизации на Spring Boot",
                 contact = @Contact(name = "Support", email = "support@example.com")
-        )
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
 )
 @SecurityScheme(
         name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
         scheme = "bearer",
-        bearerFormat = "JWT"
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
 )
 public class OpenApiConfig {
 }
